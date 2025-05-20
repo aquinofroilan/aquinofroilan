f9469baeeb7cb799a695ba98037f7078e0cc6739
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-05-20 12:28:00 +0800

    Add CertificationCard component with links and enhance BentoGridItem styling

--- components/organisms/certification-card.tsx
+++ components/organisms/certification-card.tsx
@@ -1,8 +1,45 @@
-import { BadgeCheckIcon } from "lucide-react";
+import { BadgeCheckIcon, ArrowRightCircle } from "lucide-react";
 import { BentoGridItem } from "@/components/ui";
+import { CertificationsListsPreview } from "@/data/certification-list-preview";
+import Link from "next/link";
 
 function CertificationCard() {
-    return <BentoGridItem icon={<BadgeCheckIcon size={25} />} title={<h1>Certifications</h1>} />;
+    return (
+        <BentoGridItem
+            icon={<BadgeCheckIcon size={20} />}
+            title={
+                <div className="flex gap-2 items-center justify-between w-full">
+                    <h1>Certifications and Badges</h1>
+                    <Link
+                        href={"/certifications"}
+                        className="text-xs md:text-base text-neutral-500 hover:text-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-200 transition duration-200 flex items-center gap-2"
+                    >
+                        View All
+                        <ArrowRightCircle size={15} />
+                    </Link>
+                </div>
+            }
+            description={
+                <div className="w-full flex flex-col gap-3">
+                    {CertificationsListsPreview.slice(0, 4).map((cert) => {
+                        return (
+                            <Link
+                                href={cert.link}
+                                target="_blank"
+                                rel="noreferrer"
+                                key={cert.link}
+                                className="flex gap-2 items-center bg-neutral-100 p-5 rounded-md hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 transition duration-200"
+                            >
+                                <div>
+                                    <h1 className="text-xs md:text-base">{cert.title}</h1>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </div>
+            }
+        />
+    );
 }
 
 export default CertificationCard;
