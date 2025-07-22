6284219fb778c528652ab070401dce3f0972d9e7
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-22 23:35:43 +0800

    Add ProjectsCard component with technology icons (#290)
    Introduce the ProjectsCard component to the Home page layout, featuring
    icons for Java, Spring Boot, and PostgreSQL. This enhances the visual
    representation of projects.

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,18 @@ import {
     TrascendingCodeCard,
     WakatimeStatsCard,
 } from "@/components/organisms/";
+import ProjectsCard from "@/components/organisms/projects-card";
 
 export default function Home() {
     return (
         <>
-            <main className="py-10 w-11/12 max-w-5xl gap-2 flex flex-col md:grid md:grid-cols-2">
+            <main className="py-10 w-11/12 max-w-7xl gap-2 flex flex-col md:grid md:grid-cols-2">
                 <ProfileCard className="h-full overflow-scroll col-span-2" />
                 <AboutCard className="h-full overflow-scroll col-span-1 row-span-2" />
                 <CertificationCard className="h-full overflow-scroll col-span-1 row-span-1" />
                 <TechStackCard className="h-full overflow-scroll col-span-1 row-span-1" />
-                <NowPlayingWidget className="h-full overflow-scroll w-full col-span-2 row-span-3" />
+                <ProjectsCard className="h-full overflow-scroll col-span-1 row-span-1" />
+                <NowPlayingWidget className="h-full overflow-scroll w-full col-span-1 row-span-1" />
                 <TrascendingCodeCard className="h-full overflow-scroll col-span-1 row-span-1" />
                 <WakatimeStatsCard className="h-full overflow-scroll row-span-3 col-span-1" />
                 <CompentenciesCard />
