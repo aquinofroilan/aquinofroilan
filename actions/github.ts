79a699e0c33d3f2100c5b2608171bd244706ddbb
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-22 23:39:01 +0800

    Add GitHub stats fetching functionality (#291)
    Introduce functionality to fetch GitHub statistics and integrate it into
    the NowPlayingWidget and WakatimeStatsCard components. This enhancement
    allows users to view their GitHub activity, including pull requests,
    issues, commits, and stars.

---
+++ actions/github.ts
@@ -0,0 +1,58 @@
+"use server";
+import { graphql } from "@octokit/graphql";
+
+const graphqlWithAuth = graphql.defaults({
+    headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+});
+
+interface GraphQLResponse {
+    user: {
+        pullRequests: { totalCount: number };
+        issues: { totalCount: number };
+        contributionsCollection: { totalCommitContributions: number };
+        repositories: {
+            nodes: { stargazerCount: number }[];
+        };
+    };
+}
+
+export const getGithubStats = async () => {
+    const data: GraphQLResponse = await graphqlWithAuth(`
+        query {
+            user(login: "${process.env.GITHUB_USERNAME}") {
+                pullRequests {
+                    totalCount
+                }
+                issues {
+                    totalCount
+                }
+                contributionsCollection {
+                    totalCommitContributions
+                }
+                repositories(first: 100) {
+                    nodes {
+                        stargazerCount
+                    }
+                }
+            }
+        }
+    `);
+
+    // Aggregate star count across repos
+    const totalStars = data.user.repositories.nodes.reduce(
+        (acc: number, repo: { stargazerCount: number }) => acc + repo.stargazerCount,
+        0,
+    );
+
+    const stats = {
+        pullRequests: data.user.pullRequests.totalCount,
+        issues: data.user.issues.totalCount,
+        commitsPastYear: data.user.contributionsCollection.totalCommitContributions,
+        stars: totalStars,
+    };
+    console.log(stats);
+
+    return stats;
+};
--- actions/index.ts
+++ actions/index.ts
@@ -1,3 +1,4 @@
 export { getAccessToken } from "./access-token";
 export { FetchWakaTimeStats } from "./wakatime";
 export { getNowPlaying } from "./spotify";
+export { getGithubStats } from "./github";
