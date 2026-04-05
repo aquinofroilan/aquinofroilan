f325cf919d8693bad2d137399dcd35037ffca0c6
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-04-05 17:10:31 +0800

    fix: remove unused GithubStatsCard component

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,6 +1,5 @@
 export { AboutCard } from "./about-me-card";
 export { TrascendingCodeCard } from "./transcending-coding-card";
-export { GithubStatsCard } from "./github-stats-card";
 export { TechStackCard } from "./tech-stack-card";
 export { CertificationCard } from "./certification-card";
 export { ProfileCard } from "./profile-card";
