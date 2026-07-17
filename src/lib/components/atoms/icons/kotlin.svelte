922b8fb92076d747af0de0e551723d00b093fcaa
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-07-17 13:26:24 +0800

    feat: convert React atom icons to Svelte SVGs

---
+++ src/lib/components/atoms/icons/kotlin.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let { class: className = '', size = 24 } = $props<{ class?: string, size?: number | string }>();
+</script>
+<svg
+            viewBox="0 0 32 32"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            class={className}
+            fill="currentColor"
+        >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <path d="M30.996 30.996h-29.992v-29.992h29.992l-14.996 14.996 14.996 14.996z"></path>{" "}
+            </g>
+        </svg>
