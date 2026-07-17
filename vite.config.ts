0eab04c5df13a7c7c631cc8e9c9e628daf4d5065
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-07-17 13:26:09 +0800

    chore: setup SvelteKit path aliases and TSConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve('./')
+		}
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
