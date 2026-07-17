a98899513b79c7adca62335bedc50383530109df
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-07-17 13:26:41 +0800

    feat: implement server-side data loaders for homepage

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,29 @@
+import { getGithubStats } from '../../actions/github';
+import { getCredlyCertifications } from '../../actions/credly';
+import { getRecentBlogPosts } from '../../actions/blog';
+import { getRecentBooks } from '../../actions/books';
+import { env } from '$env/dynamic/private';
+
+export const load = async () => {
+    const [githubStats, credlyCerts, posts, books] = await Promise.all([
+        getGithubStats().catch(() => null),
+        getCredlyCertifications().catch(() => []),
+        getRecentBlogPosts(4).catch(() => []),
+        getRecentBooks(4).catch(() => [])
+    ]);
+
+    return {
+        githubStats,
+        credlyCerts,
+        posts,
+        books,
+        env: {
+            CURRENT_TITLE: env.CURRENT_TITLE,
+            CURRENT_COMPANY: env.CURRENT_COMPANY,
+            CURRENT_WORK_LOCATION: env.CURRENT_WORK_LOCATION,
+            SCHEDULE_A_CALL_URL: env.SCHEDULE_A_CALL_URL,
+            SEND_EMAIL_URL: env.SEND_EMAIL_URL,
+            RESUME_CV_LINK: env.RESUME_CV_LINK,
+        }
+    };
+};
